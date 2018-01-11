0c00c396af14a4ed35aacb2cf377efb2585efdb5
Unknown <narioinc89@gmail.com>
2018-01-11 13:36:31 +0530

    Changes to pin config in BOARDS.py

--- boards/LORABLE.py
+++ boards/LORABLE.py
@@ -130,8 +130,6 @@ def get_pins():
   pins = pinutils.generate_pins(0,31) # 32 General Purpose I/O Pins.
   pinutils.findpin(pins, "PD0", True)["functions"]["XL1"]=0;
   pinutils.findpin(pins, "PD1", True)["functions"]["XL2"]=0;
-  pinutils.findpin(pins, "PD9", True)["functions"]["NFC1"]=0;
-  pinutils.findpin(pins, "PD10", True)["functions"]["NFC2"]=0;
   pinutils.findpin(pins, "PD2", True)["functions"]["ADC1_IN0"]=0;
   pinutils.findpin(pins, "PD3", True)["functions"]["ADC1_IN1"]=0;
   pinutils.findpin(pins, "PD4", True)["functions"]["ADC1_IN2"]=0;
@@ -139,9 +137,7 @@ def get_pins():
   pinutils.findpin(pins, "PD28", True)["functions"]["ADC1_IN4"]=0;
   pinutils.findpin(pins, "PD28", True)["functions"]["USART1_TX"]=0;
   pinutils.findpin(pins, "PD29", True)["functions"]["USART1_RX"]=0;
-  pinutils.findpin(pins, "PD29", True)["functions"]["ADC1_IN5"]=0;
-  pinutils.findpin(pins, "PD30", True)["functions"]["ADC1_IN6"]=0;
-  pinutils.findpin(pins, "PD31", True)["functions"]["ADC1_IN7"]=0;
+
   # everything is non-5v tolerant
   for pin in pins:
     pin["functions"]["3.3"]=0;
