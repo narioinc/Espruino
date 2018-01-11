cac5af8e3817e63b1a080b7bfca50584ef4eb67f
Unknown <narioinc89@gmail.com>
2018-01-11 11:49:18 +0530

    Initial support for LoraB module
    Added boards.py 
    Added dummy implementation for LoraBLE module in lorab folder

---
+++ libs/lorab/jswrap_lorab.c
@@ -0,0 +1,29 @@
+/*
+ * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+ *
+ * Copyright (C) 2013 Gordon Williams <gw@pur3.co.uk>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ----------------------------------------------------------------------------
+ * This file is designed to be parsed during the build process
+ *
+ * Contains JavaScript interface for Puck.js
+ * ----------------------------------------------------------------------------
+ */
+
+
+#include "jswrap_lorab.h"
+#include "jsinteractive.h"
+#include "jsdevices.h"
+#include "jshardware.h"
+#include "jsdevices.h"
+#include "jspin.h"
+#include "jstimer.h"
+#include "jswrap_bluetooth.h"
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
+#include "nrf5x_utils.h"
+#include "jswrap_flash.h" // for jsfRemoveCodeFromFlash
